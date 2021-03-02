5d41ab5b5fc86dc8128be9689b7ff3a8e9a898ad
Sonia Reuter <47145589+Sreuter29@users.noreply.github.com>
2021-03-02 10:29:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Mini-chat</title>
+</head>
+<style>
+body {
+   background-color: #aac89b;
+}
+form, h1 {
+  text-align:center;
+}
+</style>
+
+<body>
+  <h1>Alimente ce mini chat en postant un message! (255 charactères maxi!)</h1>
+  <form action="minichat_post.php" method="post">
+    <p>
+      <label for="pseudo">Pseudo :</label> <input type="text" name="pseudo" id="pseudo"><br>
+      <label for="message">Message :</lab#ecedcbel>  <textarea name="message" id="message" maxlength="255"></textarea><br>
+
+      <input type="submit" value="Envoyer">
+    </p>
+  </form>
+
+  <?php
+  // Connexion à la base de données
+  try
+  {
+    $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'root');
+  }
+  catch(Exception $e)
+  {
+    die('Erreur : '. $e->getMessage());
+  }
+
+  // Récupération des 10 derniers messages
+  $reponse = $bdd->query('SELECT pseudo, message FROM chat ORDER BY ID DESC LIMIT 0, 10');
+
+  // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+  while ($donnees = $reponse->fetch())
+  {
+    echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+  }
+
+  $reponse->closeCursor();
+
+  ?>
+</body>
+</html>
